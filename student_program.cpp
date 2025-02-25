67294013724d949457ac169e9a930e5352a95571
Natnicha♥ <142340818+Natnicha2803@users.noreply.github.com>
2025-02-25 21:13:34 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[]) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    printf( "Enter str1 : " );
+    fgets( text, sizeof(text), stdin ) ; //รับค่า
+
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
 }//end function
+
+char str2[ 50 ] ;
+char* reverse( char str1[]) {
+    int length ;
+    length = strlen( str1 ) ;
+
+    for ( int i = 0 ; i < length ; i++ ) {
+        str2[i] = str1[ length - 1 - i ] ;
+    }//end for
+    str2[ length ] = '\0' ;
+    printf( "FUNC : %s\n",str2) ;
+    return str2 ;
+}
